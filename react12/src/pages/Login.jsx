43cdf94d778b42545a00b8f889e2462ee5f244ee
Taswoor2507 <taswoorhossein72@gmail.com>
2025-05-02 17:29:36 +0500

    class12 protected routes

---
+++ react12/src/pages/Login.jsx
@@ -0,0 +1,38 @@
+import React, { useContext } from 'react'
+import { authContext } from '../AuthContext';
+import { Navigate, useNavigate } from 'react-router-dom';
+
+const Login = () => {
+   const navigate = useNavigate()
+    const {login} = useContext(authContext)
+function handleSubmit(e){
+    e.preventDefault();
+    // formData
+   const formdata = new FormData(e.target);
+   const username = formdata.get("username")
+   const password =  formdata.get("password");
+
+   if(!username || !password){
+    return
+   }
+
+   login({name:username , password:password})
+//    Navigate("/dashboard")
+navigate("/dashboard")
+   console.log("form submit")
+}
+
+
+  return (
+    <div>
+        <h1>Login</h1>
+        <form action=""  onSubmit={handleSubmit}>
+            <input type='name'   name='username' />
+            <input type='password'  name='password'  />
+            <button>Login</button>
+        </form>
+    </div>
+  )
+}
+
+export default Login
