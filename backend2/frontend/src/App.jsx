fbc681a57ed91cfc0db134539dd110f19ce1c8ff
Taswoor2507 <taswoorhossein72@gmail.com>
2025-05-06 18:04:26 +0500

    backend class 2 cors concept split modules

--- backend2/frontend/src/App.jsx
+++ backend2/frontend/src/App.jsx
@@ -1,10 +1,9 @@
 import React, { useEffect } from 'react'
 
 const App = () => {
-
   useEffect(()=>{
-    fetch("http://localhost:4040/").then(res=>res.json()).then(data=>console.log(data))
-  }, [])
+     fetch("http://localhost:6060").then(res=>res.json()).then(data=>console.log(data))
+  },[])
   return (
     <div>App</div>
   )
