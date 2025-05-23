c172bfb617790faaf138d1b6c5b95d57c80db0bc
Taswoor2507 <taswoorhossein72@gmail.com>
2025-05-23 18:45:29 +0500

    <DAY 9> refresh token logic complete

--- sass/backend/src/middlewares/verifyToken.middleware.js
+++ sass/backend/src/middlewares/verifyToken.middleware.js
@@ -1,33 +1,64 @@
 import CustomError from "../utils/CustomError.js";
 import jwt from "jsonwebtoken";
-function verifyToken(req,res,next){
-  const token  = req.cookies.token;
-  if(!token){
-    return next(new CustomError("Token not found", 401));
-  }
+// function verifyToken(req,res,next){
+//   const token  = req.cookies.token;
+//   if(!token){
+//     return next(new CustomError("Token not found", 401));
+//   }
 
-  try {
-      const decode =  jwt.verify(token , process.env.JWT_SECRET);
-      if(decode){
-        req.user = decode;
-        next();
-      }
-  } catch (error) {
+//   try {
+//       const decode =  jwt.verify(token , process.env.JWT_SECRET);
+//       if(decode){
+//         req.user = decode;
+//         next();
+//       }
+//   } catch (error) {
    
-        // if token is expired
-        if(error.name === "TokenExpiredError"){
-          return next(new CustomError("Token is expired" , 401));
-        }
+//         // if token is expired
+//         if(error.name === "TokenExpiredError"){
+//           return next(new CustomError("Token is expired" , 401));
+//         }
 
+//       return next(new CustomError("Token is not valid" , 401));
+//   }
+
+// //    {id:1,  email:"ewqewe}
+
+
+
+
+
+// }
+
+// headeers
+function verifyToken(req,res,next){
+const bearerToken  =  req.headers.authorization;
+console.log(req.headers , "HEADERSSSSSSSSSSSSS")
+console.log("TOKEN FROM HEADER" , bearerToken)
+   if(!bearerToken){
+     return next(new CustomError("Token not found", 401));
+   }
+
+   try {
+    const token = bearerToken.replace("Bearer " , "");
+    console.log(token , "BR")
+    console.log("env variable " , process.env.JWT_SECRET)
+    const decode  = jwt.verify(token , process.env.JWT_SECRET);
+    console.log(decode , "DECODED TOKEN")
+    if(!decode){
       return next(new CustomError("Token is not valid" , 401));
-  }
-
-//    {id:1,  email:"ewqewe}
-
-
+    }
+    req.user = decode;
+    next();
 
 
-
+   } catch (error) {
+    // expiry token error handling 
+    if(error.name === "TokenExpiredError"){
+      return next(new CustomError("Token is expired" , 403));
+    }
+      return next(new CustomError("Token is not valid" , 405));
+   }
 }
 
 export default verifyToken;
