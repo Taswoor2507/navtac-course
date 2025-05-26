b1065cd0195831bc17661b335b25ba0e48466dff
Taswoor2507 <taswoorhossein72@gmail.com>
2025-05-26 16:50:21 +0500

    <DAY 10> use axios

--- sass/frontend/src/authContext/AuthContext.jsx
+++ sass/frontend/src/authContext/AuthContext.jsx
@@ -1,49 +1,27 @@
 import { createContext, useState } from "react";
 
-const AuthContext = createContext(null);
+const AuthContext  = createContext(null);
+
 
 // provider
 
-function AuthProvider({ children }) {
-  const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [isLogin, setIsLogin] = useState(false);
+function AuthProvider({children}){
+ const persistValue =  JSON.parse(localStorage.getItem("persist")) || true
 
-  // handle login
-  async function handleLogin(email, password) {
-    setLoading(true);
-    try {
-      const response = await fetch("http://localhost:7070/api/v1/owner/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        credentials: "include", // Add this to handle cookies
-        body: JSON.stringify({ email, password }),
-      });
 
-      const data = await response.json();
+  const [auth , setAuth] = useState({});
+  const [persist , setPersist] = useState(persistValue)
 
-      if (!response.ok) {
-        throw new Error(data.message || "Login failed");
-      }
 
-      setUser(data.data.user);
-      setIsLogin(true);
-      return response; // Return the response for the component
-    } catch (err) {
-      console.error(err);
-      throw err; // Re-throw to handle in the component
-    } finally {
-      setLoading(false);
-    }
-  }
 
-  return (
-    <AuthContext.Provider value={{ handleLogin, isLogin, user, loading }}>
+  return(
+    <AuthContext.Provider value={{auth, setAuth, persist, setPersist}}>
       {children}
     </AuthContext.Provider>
-  );
+  )
 }
 
+
 export { AuthContext, AuthProvider };
+
+
